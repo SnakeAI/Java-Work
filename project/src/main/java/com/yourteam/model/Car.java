5243bde424d171dfbf1040b5f74f6acbb2b648dd
Danil Kuzminih <kuzminih1danil11@gmail.com>
2026-02-18 17:21:55 +0500

    Добавил свой код

--- project/src/main/java/com/yourteam/model/Car.java
+++ project/src/main/java/com/yourteam/model/Car.java
@@ -1,7 +1,34 @@
 package com.yourteam.model;
 
 public class Car {
-    public int power;
-    public String model;
-    public int year;
+    private int power, year;
+    private String model;
+
+    private Car(Builder builder) {
+        this.power = builder.power;
+        this.year = builder.year;
+        this.model = builder.model;
+    }
+    public int getPower() { return power; }
+    public int getYear() { return year; }
+    public String getModel() { return model; }
+
+    public static class Builder {
+        private int power, year;
+        private String model;
+
+        public Builder power(int power) {
+            this.power = power;
+            return this;
+        }
+        public Builder year(int year) {
+            this.year = year;
+            return this;
+        }
+        public Builder model(String model) {
+            this.model = model;
+            return this;
+        }
+    }
 }
+
--- project/src/main/java/com/yourteam/validation/CarValidator.java
+++ project/src/main/java/com/yourteam/validation/CarValidator.java
@@ -1,6 +1,18 @@
 package com.yourteam.validation;
 
-public class CarValidator extends Car {
+public final class CarValidation {
+    private CarValidation() {}
 
-
+    public static void validate(Car.Builder b) {
+        if (b.power <= 0) {
+            throw new IllegalStateException("Power must be > 0");
+        }
+        if (b.year < 1900 || b.year > 2027) {
+            throw new IllegalStateException("Year is invalid");
+        }
+        if (b.model == null || b.model.isBlank()) {
+            throw new IllegalStateException("Model is required");
+        }
+    }
 }
+
